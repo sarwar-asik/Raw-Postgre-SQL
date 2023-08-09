e05025f8edaa19ae44df1a346340b6f18430de7e
sarwar-asik <sarwarasik@gmail.com>
2023-08-09 23:32:17 +0600

    added ts

---
+++ postgre/data.ts
@@ -0,0 +1,12 @@
+type IDataTYPE={
+    name:string,
+    email:string,
+    number:number
+} 
+
+
+const user:IDataTYPE ={
+    name:"MR Johe",
+    email:"johe@gamil.com",
+    number:2312424412
+}
