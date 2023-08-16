ebc166e46e8154e08d78d5beab0bb6c31038ba0e
sarwar-asik <sarwarasik@gmail.com>
2023-08-16 11:38:40 +0600

    query-2 done

--- PostgreSQL_Assignment.sql
+++ PostgreSQL_Assignment.sql
@@ -69,6 +69,10 @@ VALUES
 SELECT * from students;
 
 --***Query 2 >>>>>>>>>>: 
+-- Retrieve the names of all students who are enrolled in the course titled 'Next.js'.
 
+SELECT stu.student_name, co.course_name from students stu
+JOIN enrollment enroll on stu.student_id = enroll.student_id
+join courses co on co.course_id = enroll.course_id
+where co.course_name = 'Next.js';
 
-SELECT
